91e0ea097dc4e56e753af0e5ff8c5e12c0c92eca
MaximilianoGarciaRoe <mgarcia@buk.cl>
2024-05-05 22:01:58 -0400

    feat: create base lab-09

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,41 @@
 class PostsController < ApplicationController
-    def index
-        @posts = Post.all
-    end
+	def index
+		@posts = Post.all
+	end
 
-    def show
-        @post = Post.find params['id']
-    end
+	def show
+		@post = Post.find(params[:id])
+	end
+
+	def new
+		@post = Post.new
+	end
+
+	def create  
+		@post = Post.new(post_params)
+		if @post.save!
+				redirect_to posts_path
+		else
+				render :new
+		end
+	end
+
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def update
+		@post = Post.find(params[:id])
+		if @post.update post_params
+				redirect_to posts_path
+		else
+				render :edit
+		end
+	end
+
+	private
+
+	def post_params
+		params.require(:post).permit(:title, :content, :user_id, :answers_count)
+	end
 end
